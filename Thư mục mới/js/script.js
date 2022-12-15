98e2288de0efb68eb9ae820b91f4382ffafff4b4
nguyenson2003 <nqson123@gmail.com>
2022-12-15 22:41:28 +0700

    Update script.js

--- Thư mục mới/js/script.js
+++ Thư mục mới/js/script.js
@@ -1,44 +1,82 @@
-// coding implementation
+// 1. PHAN CODE CO THE SU DUNG TRONG DE THI
+function validate_bo_trong(e) {
+    if (e.value.length == 0) {
+        return true;
+    }
+    return false;
+}
+
+function validate_rating(e) {
+    let num = e.value;
+    if (isNaN(parseInt(num))) return true;
+    return false;
+}
+
+function checkEmail(e) {
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9])+$/;
+    if (!filter.test(e.value)) {
+        return true;
+    }
+    return false
+}
+
+//vi du kiem tra xau chu dau khong fai so, cac chu khong phai ky tu dac biet
+// true: xau ko hop le, false: xau hop le
+function custom(e) { 
+    let str = e.value;
+    let n = str.length;
+    if(str[0]<'0' && '9'<str[0]) return true;
+    for(let i = 0;i<n;i++){
+        let z = true;
+        if('0'<=str[i] && str[i]<='9') z = false;
+        if('a'<=str[i] && str[i]<='z') z = false;
+        if('A'<=str[i] && str[i]<='Z') z = false;
+        if(z) return true;
+    }
+
+    return false;
+}
+
+// 2. PHAN CODE TRONG DE THI
+
+// (bắt buộc fai gõ)
 document.body.onload =
-    function test() {
+    () => {
+        //const submit = document.getElementById("ten_id_cua_nut_submit");
         const submit = document.getElementById("submit");
-        console.log(submit);
-        submit.setAttribute("type","button")
-        submit.onclick = 
-            function test2(){
-                const email = document.getElementById("email").value;
-                console.log(email)
-                if(email.length == 0){
-                    alert("chua nhap email")
-                    return null;
-                }else{
-                    let temp = email.indexOf("@")
-                    if(temp<1){
-                        alert("email sai dinh dang")
-                        return null;
-                    }
-                    let temp2 = email.indexOf(".");
-                    if(temp2<0 || temp>=temp2-1 || temp2>=email.length-1){
-                        alert("email sai dinh dang")
-                        return null;
-                    }
+        submit.setAttribute("type", "button")
+        submit.onclick =
+            () => {
+
+                //tuy theo de bai
+
+                //function validate bo trong duoc viet ở muc 1
+                // validate_bo_trong(document.getElementById("ten_id_ktra_validate_bo_trong"))
+                if (validate_bo_trong(document.getElementById("email"))) {
+                    alert("chua dien email")
+                    return;
                 }
-                const cmt = document.getElementById("com").value;
-                if(cmt.length==0){
-                    alert("chua nhap comment")
-                    return null;
+                if (validate_bo_trong(document.getElementById("com"))) {
+                    alert("chua dien comment")
+                    return;
                 }
-                
-                const rate = document.getElementById("rate").value;
-                if(rate.length== 0){
-                    alert("chua nhap rate")
-                    return null;
-                }else if(parseInt(rate) !== Number(rate)){
-                    alert("rate dinh dang so")
-                    return null;
+                if (validate_bo_trong(document.getElementById("rate"))) {
+                    alert("chua dien rate")
+                    return;
                 }
+
+                //validate_rating(document.getElementById("ten_id_ktra_dinh_dang_so"))
+                if (validate_rating(document.getElementById("rate"))) {
+                    alert("rate phai la mot so")
+                    return;
+                }
+
+                //
+                if (checkEmail(document.getElementById("email"))) {
+                    alert("email khong dung dinh dang")
+                    return;
+                }
+                //bat buoc
                 location.reload()
             }
     }
-
-// end coding implementation
